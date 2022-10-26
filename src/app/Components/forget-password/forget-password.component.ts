0b59b7c5d3137b0fb012bb5aca53a986eac5a72b
Bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-10-26 15:20:29 +0530

    applyting validation for forget password page

--- src/app/Components/forget-password/forget-password.component.ts
+++ src/app/Components/forget-password/forget-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forget-password.component.scss']
 })
 export class ForgetPasswordComponent implements OnInit {
+  forgotpasswordForm!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.forgotpasswordForm = this.formBuilder.group({
+      email: ['', Validators.required],
+  });
   }
-
+  onSubmit(){
+    this.submitted = true;
+    if (this.forgotpasswordForm.invalid) {
+      return;
+  }
+  }
 }
