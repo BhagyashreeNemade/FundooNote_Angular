888079ef46091b5038a0e0019c565ac3591ec529
Bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-10-26 14:26:15 +0530

    routing link added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import {  Routes,RouterModule} from '@angular/router';
+import { RegisterComponent } from './Components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
 
+const routes: Routes = [
+  {path:'registration',component:RegisterComponent},
+   {path:'login',component:LoginComponent},
 
-const routes: Routes = [];
+{path:'forget-pass',component:ForgetPasswordComponent},
+{path:'reset-password/:token',component:ResetPasswordComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
