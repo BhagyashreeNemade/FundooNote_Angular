4123c1882a79216ec7e86a9968c022e7110f61df
Bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-10-26 15:21:15 +0530

    applyting validation for reset password page

--- src/app/Components/reset-password/reset-password.component.ts
+++ src/app/Components/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
+  resetpasswordForm!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.resetpasswordForm = this.formBuilder.group({
+      Newpassword: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.resetpasswordForm.invalid) {
+      return;
+  }
+
   }
 
 }
